48d287369bf1b79809dc687c84b8e6686a9376b8
Cris <silvestro.cristian@gmail.com>
2023-03-14 19:37:19 -0300

    Commit n° 9 - Testing with postman E401, searching for a solution

--- src/main/java/com/icon/Practice/Entity/IconEntity.java
+++ src/main/java/com/icon/Practice/Entity/IconEntity.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class IconEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private String photo;
